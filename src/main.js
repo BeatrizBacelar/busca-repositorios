392277a7ccb4cb26aae8578864b858935ef0c363
BeatrizBacelar <beabacelar18@gmail.com>
2021-06-20 22:12:27 -0300

    feat: renderização da tela

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ class App{
 
         //Form
         this.formulario = document.querySelector('form');
+
+        //lista
+        this.lista = document.querySelector('.list-group');
         
         //Método para registrar os eventos do form
         this.registrarEventos();
@@ -27,7 +30,54 @@ class App{
             link: 'https://github.com/ryanoasis/nerd-fonts',
         });
         // renderizar a tela
-        console.log(this.repositorios);
+        this.renderizarTela();
+        console.log(this.lista);
+    }
+
+    renderizarTela(){
+        //limpar o conteudo de lista
+        this.lista.innerHTML = '';
+
+        // percorrer toda a lista de repositorios e criar os elementos
+        this.repositorios.forEach(repositorios => {
+            // <li>
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-action');
+
+            //<img>
+            let img = document.createElement('img');
+            img.setAttribute('src', repositorio.avatar_url);
+            li.appendChild(img);
+
+            //<strong>
+            let strong = document.createElement('strong');
+            let txtNome = document.createTextNode(repositorio.nome);
+            strong.appendChild(txtNome)
+            li.appendChild(strong);
+
+            //<p>
+            let p = document.createElement('p');
+            let txtDescricao = document.createTextNode(repositorio.descricao);
+            p.appendChild(txtDescricao);
+            li.appendChild(p);
+
+            //<a>
+            let a = document.createElement('a');
+            a.setAttribute('target', '_blank');
+            a.setAttribute('href', repositorio.link);
+            let txtA = document.createTextNode('Acessar');
+            a.appendChild(txtA);
+            li.appendChild(a);
+
+            //Adicionar <li> como filho da ul
+            this.lista.appendChild(li);
+
+            //Limpar o conteudo do input
+            this.formulario.querySelector('input[id=repositorio]').value = '';
+
+            // Adiciona o foco no input
+            this.formulario.querySelector('input[id=repositorio]').focus();
+        });
     }
 }
 
